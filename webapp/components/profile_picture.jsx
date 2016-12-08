b66686a6d1d1928593fa8ebf1fa76ff712b90acd
David Meza <dmeza@ext.uber.com>
2016-12-08 00:41:30 -0500

    UCHAT-181 Add button to DM a user from their profile preview popover tile

--- webapp/components/profile_picture.jsx
+++ webapp/components/profile_picture.jsx
@@ -1,12 +1,27 @@
 // Copyright (c) 2016 Mattermost, Inc. All Rights Reserved.
 // See License.txt for license information.
+import SpinnerButton from 'components/spinner_button.jsx';
+import {openDirectChannelToUser} from 'actions/channel_actions.jsx';
+import {browserHistory} from 'react-router/es6';
+
 import * as Utils from 'utils/utils.jsx';
 import * as PostUtils from 'utils/post_utils.jsx';
 import UserStore from 'stores/user_store.jsx';
 import React from 'react';
 import {Popover, OverlayTrigger} from 'react-bootstrap';
+import {FormattedMessage} from 'react-intl';
 
 export default class ProfilePicture extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.handleShowDirectChannel = this.handleShowDirectChannel.bind(this);
+        this.state = {
+            currentUserId: UserStore.getCurrentId(),
+            loadingDMChannel: false
+        };
+    }
+
     shouldComponentUpdate(nextProps) {
         if (nextProps.src !== this.props.src) {
             return true;
@@ -29,6 +44,30 @@ export default class ProfilePicture extends React.Component {
         }
 
         return false;
+    }
+
+    handleShowDirectChannel(teammate, e) {
+        e.preventDefault();
+
+        if (this.state.loadingDMChannel) {
+            return;
+        }
+
+        this.setState({loadingDMChannel: true});
+        openDirectChannelToUser(
+            teammate,
+            (channel) => {
+                this.exitToDirectChannel = TeamStore.getCurrentTeamRelativeUrl() + '/channels/' + channel.name;
+                this.setState({loadingDMChannel: false});
+                this.refs.overlay.hide();
+                if (this.exitToDirectChannel) {
+                    browserHistory.push(this.exitToDirectChannel);
+                }
+            },
+            () => {
+                this.setState({loadingDMChannel: false});
+            }
+        );
     }
 
     render() {
@@ -85,8 +124,25 @@ export default class ProfilePicture extends React.Component {
                     </div>
                 );
             }
+            if (this.props.user.id !== this.state.currentUserId) {
+                dataContent.push(
+                    <div className='msg-btn-container'>
+                        <SpinnerButton
+                            className='btn btm-sm btn-primary'
+                            spinning={this.state.loadingDMChannel}
+                            onClick={this.handleShowDirectChannel.bind(this, this.props.user)}
+                        >
+                            <FormattedMessage
+                                id='more_direct_channels.message'
+                                defaultMessage='Message'
+                            />
+                        </SpinnerButton>
+                    </div>
+                );
+            }
             return (
                 <OverlayTrigger
+                    ref="overlay"
                     className='hidden-xs'
                     trigger='click'
                     placement='right'
--- webapp/components/user_profile.jsx
+++ webapp/components/user_profile.jsx
@@ -1,6 +1,10 @@
 // Copyright (c) 2015 Mattermost, Inc. All Rights Reserved.
 // See License.txt for license information.
 
+import SpinnerButton from 'components/spinner_button.jsx';
+import {openDirectChannelToUser} from 'actions/channel_actions.jsx';
+import {browserHistory} from 'react-router/es6';
+
 import * as Utils from 'utils/utils.jsx';
 import Client from 'client/web_client.jsx';
 import UserStore from 'stores/user_store.jsx';
@@ -21,8 +25,10 @@ export default class UserProfile extends React.Component {
         super(props);
 
         this.initWebrtc = this.initWebrtc.bind(this);
+        this.handleShowDirectChannel = this.handleShowDirectChannel.bind(this);
         this.state = {
-            currentUserId: UserStore.getCurrentId()
+            currentUserId: UserStore.getCurrentId(),
+            loadingDMChannel: false
         };
     }
 
@@ -67,6 +73,30 @@ export default class UserProfile extends React.Component {
         }
 
         return false;
+    }
+
+    handleShowDirectChannel(teammate, e) {
+        e.preventDefault();
+
+        if (this.state.loadingDMChannel) {
+            return;
+        }
+
+        this.setState({loadingDMChannel: true});
+        openDirectChannelToUser(
+            teammate,
+            (channel) => {
+                this.exitToDirectChannel = TeamStore.getCurrentTeamRelativeUrl() + '/channels/' + channel.name;
+                this.setState({loadingDMChannel: false});
+                this.refs.overlay.hide();
+                if (this.exitToDirectChannel) {
+                    browserHistory.push(this.exitToDirectChannel);
+                }
+            },
+            () => {
+                this.setState({loadingDMChannel: false});
+            }
+        );
     }
 
     render() {
@@ -202,8 +232,26 @@ export default class UserProfile extends React.Component {
             );
         }
 
+        if (this.props.user.id !== this.state.currentUserId) {
+            dataContent.push(
+                <div className='msg-btn-container'>
+                    <SpinnerButton
+                        className='btn btm-sm btn-primary'
+                        spinning={this.state.loadingDMChannel}
+                        onClick={this.handleShowDirectChannel.bind(this, this.props.user)}
+                    >
+                        <FormattedMessage
+                            id='more_direct_channels.message'
+                            defaultMessage='Message'
+                        />
+                    </SpinnerButton>
+                </div>
+            );
+        }
+
         return (
             <OverlayTrigger
+                ref="overlay"
                 className='hidden-xs'
                 trigger='click'
                 placement='right'
